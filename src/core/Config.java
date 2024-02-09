ebb25c778c1e38faf954c4451202de3f5acc8354
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-02-09 21:25:35 +0300

    project-2

---
+++ src/core/Config.java
@@ -0,0 +1,7 @@
+package core;
+public class Config {
+    public final static String PROJECT_TITLE = "Patika Tourism Agency";
+    public final static String DB_URL = "jdbc:mysql://localhost/trsm";
+    public final static String DB_USERNAME = "root";
+    public final static String DB_PASSWORD = "mysql";
+}
